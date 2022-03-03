46c14594107bd8e35d829e5d21782c2f44a089c6
masuzi <mas.uziku@gmail.com>
2022-03-03 23:38:16 +0000

    cors

--- src/index.js
+++ src/index.js
@@ -29,7 +29,9 @@ const parser = multer({ storage });
 // Rsegister plugins below:
 // register fastify content parser
 fastify.register(db, { uri });
-fastify.register(cors, { origin: "http://localhost:3000" });
+fastify.register(cors, {
+  origin: ["http://localhost:3000", "https://simantep.maannawawi.sch.id"],
+});
 fastify.register(multer.contentParser);
 fastify.register(require("./routes/status"));
 fastify.register(require("./routes/gallery"));
