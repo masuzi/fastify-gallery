b956ada518d638b35503887335bb4613db1f1d51
masuzi <mas.uziku@gmail.com>
2022-03-03 16:12:37 +0000

    fix

--- src/config/index.js
+++ src/config/index.js
@@ -1,26 +1,26 @@
-const fp = require('fastify-plugin');
-const mongoose = require('mongoose');
-const Gallery = require('./models/gallery');
+const fp = require("fastify-plugin");
+const mongoose = require("mongoose");
+const Gallery = require("./models/gallery");
 
 const models = { Gallery };
 
 const ConnectDB = async (fastify, options) => {
-	try {
-		mongoose.connection.on('connected', () => {
-			fastify.log.info({ actor: 'MongoDB' }, 'connected');
-		});
-		mongoose.connection.on('disconnected', () => {
-			fastify.log.error({ actor: 'MongoDB' }, 'disconnected');
-		});
-		const db = await mongoose.connect(options.uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true
-		});
+  try {
+    mongoose.connection.on("connected", () => {
+      fastify.log.info({ actor: "MongoDB" }, "connected");
+    });
+    mongoose.connection.on("disconnected", () => {
+      fastify.log.error({ actor: "MongoDB" }, "disconnected");
+    });
+    const db = await mongoose.connect(options.uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-		fastify.decorate('db', { models });
-	} catch (error) {
-		console.error(error);
-	}
+    fastify.decorate("db", { models });
+  } catch (error) {
+    console.error(error);
+  }
 };
 module.exports = fp(ConnectDB);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-require('dotenv').config();
-const fastify = require('fastify')({ logger: true });
-const cors = require('fastify-cors');
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('fastify-multer');
-const db = require('./config/index');
+require("dotenv").config();
+const fastify = require("fastify")({ logger: true });
+const cors = require("fastify-cors");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("fastify-multer");
+const db = require("./config/index");
 const Port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
 // cloudinary configuration
 cloudinary.config({
-	cloud_name: process.env.CLOUD_NAME,
-	api_key: process.env.CLOUD_KEY,
-	api_secret: process.env.CLOUD_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-	cloudinary: cloudinary,
-	params: {
-		folder: 'fastify-gallery',
-		allowedFormats: [ 'jpg', 'png' ],
-		transformation: [ { width: 800, height: 800, crop: 'limit' } ]
-	}
+  cloudinary: cloudinary,
+  params: {
+    folder: "test_up",
+    allowedFormats: ["jpg", "png"],
+    // transformation: [{ width: 800, height: 800, crop: "limit" }],
+  },
 });
 
 const parser = multer({ storage });
@@ -29,20 +29,20 @@ const parser = multer({ storage });
 // Rsegister plugins below:
 // register fastify content parser
 fastify.register(db, { uri });
-fastify.register(cors, { origin: 'http://localhost:3000' });
+fastify.register(cors, { origin: "http://localhost:3000" });
 fastify.register(multer.contentParser);
-fastify.register(require('./routes/status'));
-fastify.register(require('./routes/gallery'));
+fastify.register(require("./routes/status"));
+fastify.register(require("./routes/gallery"));
 
-fastify.decorate('multer', { parser });
+fastify.decorate("multer", { parser });
 
 const start = async () => {
-	try {
-		await fastify.listen(Port);
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+  try {
+    await fastify.listen(Port);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
